63e5034a55be52347b580b737b43431a1aa5cbcd
hyrudotcom <hyrudotcom@protonmail.com>
2025-01-31 11:01:47 +0100

    setup shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ export default function Home() {
   return (
     <>
       <h1 className="text-3xl text-primary">welcome to nextjs</h1>
-      <h1 className="text-3xl font-inter">welcome to nextjs(inter)</h1>
-      <h1 className="text-3xl font-space-grotesk">welcome to nextjs(inter)</h1>
     </>
   );
 }
