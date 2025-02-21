6ce0aed0d5986f953363fcdddcb1042f44fd1700
hyrudotcom <hyrudotcom@protonmail.com>
2025-02-21 15:00:09 +0100

    stash

---
+++ components/navigation/navbar/MobileNavigation.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const MobileNavigation = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Image src="/icons/hamburger.svg" />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNavigation;
